51ece14ceb347b4609e3a600370ce342ef66d9d5
Autarc <autarc@gmail.com>
2016-01-05 14:25:11 +0100

    add examples

---
+++ example/basic-server.js
@@ -0,0 +1,17 @@
+
+var fs = require('fs')
+
+var React = require('react')
+var renderToString = require('react-dom/server').renderToString
+var ReactHTMLTree = require('./lib/')
+
+
+var tree = React.createElement(ReactHTMLTree, {
+  source: '<body><h2>ReactHTMLTree</h2> <a href="http://github.com/autarc/react-htmltree">Repository</a></body>'
+})
+
+var html = renderToString(tree)
+
+console.log(html);
+
+fs.writeFile(__dirname + '/basic-server-generated.html', html)
